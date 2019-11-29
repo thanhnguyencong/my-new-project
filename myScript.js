8634bb0f46743b853cbcfd802ae92629465da9d3
thanhnguyencong <saintValentine9657@gmail.com>
2019-11-29 16:13:41 +0700

    -them thuc hanh su dung toan tu - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -bt can nang - -
    -
    -
    -
    -

---
+++ myScript.js
@@ -0,0 +1,8 @@
+let inputWidth
+let inputHeight
+inputWidth = prompt("enter the width")
+inputHeight = promp("enter the height")
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("the area is:" + area);
